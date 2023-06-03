aa38b02fb7eeefc00bca64981c581eaf21a2ff09
turnerandy17 <turnerandy17@gmail.com>
2023-06-03 14:54:02 -0700

    Initial Commit

---
+++ 5305-Final-Code.R
@@ -0,0 +1,23 @@
+# Matthew Brodie, Monica Cao, Andy Turner
+# OMSBA 5305 - DTC
+# 6/3/2/3
+
+# Import Packages
+library(readxl)
+library(dplyr)
+library(forcats)
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(forecast)
+library(dynlm)
+library(tseries)
+library(stats)
+library(TidyDensity)
+
+# Import Data
+
+
+
+
+
