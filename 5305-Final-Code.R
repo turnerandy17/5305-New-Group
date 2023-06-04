bf1e10b657e6c21cab5b63ca31c2e6df522767b0
turnerandy17 <turnerandy17@gmail.com>
2023-06-03 17:57:01 -0700

    Added complex models

--- 5305-Final-Code.R
+++ 5305-Final-Code.R
@@ -78,32 +78,164 @@ time_series %>% ur.kpss() %>% summary()
 ts_log %>% ur.kpss() %>% summary()
 
 
-# ACF and PACF -----------------------------------------------------------------
-# ACF and PACF Time Series
+# ACF, PACF, White-Noise -------------------------------------------------------
+# Time Series
 acf(time_series, lag.max = 20, plot = TRUE)
 acf(time_series, lag.max = 20, plot = FALSE) # prints autocorrelations
 pacf(time_series, lag.max = 20, plot = TRUE)
 pacf(time_series, lag.max = 20, plot = FALSE) # prints autocorrelations
+Box.test(time_series, type = "Ljung-Box")
 
-# ACF and PACF Logged Time Series
+# Logged Time Series
 acf(ts_log, lag.max = 20, plot = TRUE)
 acf(ts_log, lag.max = 20, plot = FALSE) # prints autocorrelations
 pacf(ts_log, lag.max = 20, plot = TRUE)
 pacf(ts_log, lag.max = 20, plot = FALSE) # prints autocorrelations
+Box.test(ts_log, type = 'Ljung-Box')
 
-# NEED TO DETERMINE WHAT ACF PACF INTEREPRETATIONS ARE...
+# NEED TO DETERMINE WHAT  INTEREPRETATIONS ARE...
+ 
 
 # Simple Forecast Models -------------------------------------------------------
+lm_1 <- lm(TB3~date, data = fcst)
+summary(lm_1)
+lm_aic <- AIC(lm_1)
+lm_bic <- BIC(lm_1) # referred to differently in class
+lm_1_plots <- checkresiduals(lm_1)
+lm_1_plots
+
+# WANT TO ADD RECURSIVE, ROLLING, FIXED, etc.
 
 
+# NOT LOGGED Complex Forecast Models (MA, AR, ARMA) ----------------------------
+# Create MA(1) Model 
+ma1 <- arima(time_series, order=c(0,0,1))
+summary(ma1)
+autoplot(ma1)
+checkresiduals(ma1)
+aic_ma1 <- AIC(ma1)
+bic_ma1 <- BIC(ma1)
+
+# Create MA(2) Model
+ma2 <- arima(time_series, order=c(0,0,2))
+summary(ma2) 
+autoplot(ma2)
+checkresiduals(ma2)
+aic_ma2 <- AIC(ma2)
+bic_ma2 <- BIC(ma2)
+
+# Create AR(1) model
+ar1 <- arima(time_series, order=c(1,0,0))
+summary(ar1)
+autoplot(ar1)
+checkresiduals(ar1)
+aic_ar1 <- AIC(ar1)
+bic_ar1 <- BIC(ar1)
+
+# Create AR(2) model
+ar2 <- arima(time_series, order=c(2,0,0))
+summary(ar2)
+autoplot(ar2)
+checkresiduals(ar2)
+aic_ar2 <- AIC(ar2)
+bic_ar2 <- BIC(ar2)
+
+# Create ARMA(1,1) model
+arma11 <- arima(time_series, order=c(1,0,1))
+summary(arma11)
+autoplot(arma11)
+checkresiduals(arma11)
+aic_arma11 <- AIC(arma11)
+bic_arma11 <- BIC(arma11)
+
+# Create ARMA(2,2) model
+arma22 <- arima(time_series, order=c(2,0,2))
+summary(arma22) 
+autoplot(arma22)
+checkresiduals(arma22)
+aic_arma22 <- AIC(arma22)
+bic_arma22 <- BIC(arma22)
+
+# Create ARMA(1,2) model
+arma12 <- arima(time_series, order=c(1,0,2))
+summary(arma12)
+autoplot(arma12)
+checkresiduals(arma12)
+aic_arma12 <- AIC(arma12)
+bic_arma12 <- BIC(arma12)
+
+# Create ARMA(2,1) model
+arma21 <- arima(time_series, order=c(2,0,1))
+summary(arma21)
+autoplot(arma21)
+checkresiduals(arma21)
+aic_arma21 <- AIC(arma21)
+bic_arma21 <- BIC(arma21)
 
 
+# LOGGED Complex Forecast Models (MA, AR, ARMA) --------------------------------
+# Create MA(1) Model 
+log_ma1 <- arima(ts_log, order=c(0,0,1))
+summary(ma1)
+autoplot(ma1)
+checkresiduals(ma1)
+aic_ma1 <- AIC(ma1)
+bic_ma1 <- BIC(ma1)
 
+# Create MA(2) Model
+log_ma2 <- arima(ts_log, order=c(0,0,2))
+summary(ma2) 
+autoplot(ma2)
+checkresiduals(ma2)
+aic_ma2 <- AIC(ma2)
+bic_ma2 <- BIC(ma2)
 
+# Create AR(1) model
+log_ar1 <- arima(ts_log, order=c(1,0,0))
+summary(ar1)
+autoplot(ar1)
+checkresiduals(ar1)
+aic_ar1 <- AIC(ar1)
+bic_ar1 <- BIC(ar1)
 
+# Create AR(2) model
+log_ar2 <- arima(ts_log, order=c(2,0,0))
+summary(ar2)
+autoplot(ar2)
+checkresiduals(ar2)
+aic_ar2 <- AIC(ar2)
+bic_ar2 <- BIC(ar2)
 
+# Create ARMA(1,1) model
+log_arma11 <- arima(ts_log, order=c(1,0,1))
+summary(arma11)
+autoplot(arma11)
+checkresiduals(arma11)
+aic_arma11 <- AIC(arma11)
+bic_arma11 <- BIC(arma11)
 
+# Create ARMA(2,2) model
+log_arma22 <- arima(ts_log, order=c(2,0,2))
+summary(arma22) 
+autoplot(arma22)
+checkresiduals(arma22)
+aic_arma22 <- AIC(arma22)
+bic_arma22 <- BIC(arma22)
 
+# Create ARMA(1,2) model
+log_arma12 <- arima(ts_log, order=c(1,0,2))
+summary(arma12)
+autoplot(arma12)
+checkresiduals(arma12)
+aic_arma12 <- AIC(arma12)
+bic_arma12 <- BIC(arma12)
 
+# Create ARMA(2,1) model
+log_arma21 <- arima(ts_log, order=c(2,0,1))
+summary(arma21)
+autoplot(arma21)
+checkresiduals(arma21)
+aic_arma21 <- AIC(arma21)
+bic_arma21 <- BIC(arma21)
 
 
