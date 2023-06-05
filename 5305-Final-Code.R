3118b8b285de7de146a1fd14b258f585f11e9afe
turnerandy17 <turnerandy17@gmail.com>
2023-06-04 21:08:30 -0700

    Commit before deleting code graveyard

--- 5305-Final-Code.R
+++ 5305-Final-Code.R
@@ -106,11 +106,12 @@ pacf(ts_log, lag.max = 20, plot = TRUE)
 pacf(ts_log, lag.max = 20, plot = FALSE) # prints autocorrelations
 Box.test(ts_log, type = 'Ljung-Box')
 
-# ACTION: NEED TO DETERMINE WHAT INTEREPRETATIONS ARE FOR ACF/PACF...
-# Monica to take this this...
+# Based on the above Our PACF plot does have a significant spike at lag 1, however since there are other
+# lags with significant spikes and the ACF plot also shows other significant spikes, we am leaning towards ARMA.
+# For the ACF and PACF interpretations, both ACF and PACF plots show significant spikes at different lags,
+# which suggests a possible ARMA model for our log time series.
 
-
-# PART 1 Forecast Models (lines 116:307)----------------------------------------
+# PART 1 Forecast Models (lines 114:170)----------------------------------------
 # Based on the ACF and PACF, you will choose three linear models (MA, AR, or ARMA) and estimate them.
 # You will present the estimation results, show the ACF and PACF correlograms of residuals from
 # each specification, and verify they are white noise using Q-Test.
@@ -120,7 +121,6 @@ Box.test(ts_log, type = 'Ljung-Box')
 
 # LOGGED Complex Forecast Models (MA, AR, ARMA) --------------------------------
 # Based on the ACF/PACF, we are focusing on the ARMA models
-
 
 # Create ARMA(1,1) model
 log_arma11 <- arima(ts_log, order=c(1,0,1))
@@ -170,31 +170,10 @@ plot(log_arma21_6mth)
 # This part was built in our slide deck...
 
 
+
 # END OF PART 1 ----------------------------------------------------------------
 # ------------------------------------------------------------------------------
-
-
-# PART 2 Full Project Spec -----------------------------------------------------
-# You will describe your forecasting environment and make the following options: one-step ahead forecast (h=1),
-# split your sample into two parts. Using the first 90% sample as estimation sample and the 
-# rest as prediction sample.
-
-# Use the fixed sampling scheme, consider at least three models (at least one of them is ARMA
-# models, which could be AR,MA, or ARMA). You will use quadratic loss function and Mean Squared
-# Error (MSE) to choose the optimal forecast. You will implement the forecast optimality tests
-# (MPE and informational efficiency tests) for each model, discard any model if necessary, and add
-# a simpler forecast that is calculated by averaging the last four observations (call it simple average
-# 4 naive model), ft,1 = (yt + yt−1 + yt−2 + yt−3) /4. 
-
-# When necessary, you implement the test of unconditional predictability and explain which forecast 
-# is preferred. You will make combined forecasts from your top three to five models, which should 
-# include ARMA models and the simple average 4 naive model. Then use three linear combination schemes:
-# 1) an equal-weighted forecast,
-# 2) a forecast that weights each individual forecast by the inverse of its MSE,
-# 3) an OLS weighted optimal forecast. 
-# You will show the weights and MSE of these three combined forecasts in a table
-# format, similar to Table 9.8 on page 246, and comment on which one you prefer.
-
+# BEGINNING OF PART 2 ----------------------------------------------------------
 
 # Out of Sample - Part 2 Split Sample into Two Parts ---------------------------
 # You will describe your forecasting environment and make the following options: one-step ahead forecast (h=1),
@@ -218,19 +197,19 @@ train_log <- ts(diff(log(fcst$TB3)), frequency = 12, start = c(2001,1))
 # a simpler forecast that is calculated by averaging the last four observations (call it simple average
 # 4 naive model), ft,1 = (yt + yt−1 + yt−2 + yt−3) /4. 
 
-# Model 1 Fixed Scheme - ARMA 2 ------------------------------------------------
+# Model 1 Fixed Scheme - AR3 ---------------------------------------------------
 # setting vectors = to 27 values because 10% of our data is 27 values
 fcst1 <- numeric(27)
 ferror1 <- numeric(27)
 loss1 <- numeric(27)
 
-model_1 <- dynlm(train_log ~ stats::lag(train_log, -1) + stats::lag(train_log, -2),
+model_1 <- dynlm(train_log ~ stats::lag(train_log, -1) + stats::lag(train_log, -3),
                start = c(2001,1),
                end = c(2020,11))
 summary(model_1)
 
 for (i in 1:27) {
-  fcst1[i] <- coef(model_1)[1] + coef(model_1)[2] * train_log[239+i] + coef(model_1)[3] * train_log[239+i] 
+  fcst1[i] <- coef(model_1)[1] + coef(model_1)[2] * train_log[238+i] + coef(model_1)[3] * train_log[236+i] 
   ferror1[i] <- train_log[239+i] - fcst1[i]
   loss1[i] <- ferror1[i]^2
 }
@@ -256,8 +235,8 @@ model_2 <- dynlm(train_log ~ stats::lag(train_log, -1),
 summary(model_2)
 
 for (i in 1:27) {
-  fcst2[i] <- coef(model_1)[1] + coef(model_1)[2] * train_log[239+i] + coef(model_1)[3] * train_log[239+i] 
-  ferror2[i] <- train_log[239+i] - fcst1[i]
+  fcst2[i] <- coef(model_2)[1] + coef(model_2)[2] * train_log[238+i]
+  ferror2[i] <- train_log[239+i] - fcst2[i]
   loss2[i] <- ferror1[i]^2
 }
 
@@ -270,28 +249,30 @@ summary(mpetest_2)
 IETest_2 <- lm(ferror2 ~ fcst2)
 summary(IETest_2) # Informal Efficiency Test Model 2
 
-# Model 3 Fixed Scheme - ARMA 1,1 ------------------------------------------------
+# Model 3 Fixed Scheme - ARMA --------------------------------------------------
 # setting vectors = to 27 values because 10% of our data is 27 values
 fcst3 <- numeric(27)
 ferror3 <- numeric(27)
 loss3 <- numeric(27)
 
-model_3 <- dynlm(train_log ~ stats::lag(train_log, -1) + stats::lag(train_log, -4),
+model_3 <- dynlm(train_log ~ stats::lag(train_log, -1) + 
+                   stats::lag(train_log, -2) +
+                   stats::lag(train_log, -4),
                  start = c(2001,1),
                  end = c(2020,11))
 summary(model_3)
 
 for (i in 1:27) {
-  fcst3[i] <- coef(model_1)[1] + coef(model_1)[2] * train_log[239+i] + coef(model_1)[3] * train_log[239+i] 
-  ferror3[i] <- train_log[239+i] - fcst1[i]
-  loss3[i] <- ferror1[i]^2
+  fcst3[i] <- coef(model_3)[1] + coef(model_3)[2] * train_log[238+i] + coef(model_3)[3] * train_log[237+i] + coef(model_3)[4] * train_log[235+i]
+  ferror3[i] <- train_log[239+i] - fcst3[i]
+  loss3[i] <- ferror3[i]^2
 }
 
 cbind(fcst3, ferror3, loss3)
 MSE3 <- mean(loss3)
 paste('MSE Model 3 Fixed Scheme: ', MSE3)
 
-mpetest_3 <- lm(ferror3 ~1)
+mpetest_3 <- lm(ferror3 ~ 1)
 summary(mpetest_3)
 IETest_3 <- lm(ferror3 ~ fcst3)
 summary(IETest_3) # Informal Efficiency Test Model 3
@@ -307,17 +288,17 @@ model_4 <- dynlm(train_log ~ stats::lag(train_log, -1) + stats::lag(train_log, -
 summary(model_4)
 
 for (i in 1:27){
-  fcst4[i] <- (train_log[239 + i] + 
-                 train_log[238 + i] +
+  fcst4[i] <- (train_log[238 + i] + 
                  train_log[237 + i] +
-                 train_log[236 + i]
+                 train_log[236 + i] +
+                 train_log[235 + i]
                  ) / 4
   ferror4[i] <- train_log[239+i] - fcst4[i]
   loss4[i] <- ferror4[i] ^ 2
 }
 
 cbind(fcst4, ferror4, loss4)
-MSE4 <- mean(loss1)
+MSE4 <- mean(loss4)
 paste('MSE Model 4 - Simple Naive Fixed Scheme: ', MSE4)
 
 mpetest_4 <- lm(ferror4 ~1)
@@ -361,35 +342,34 @@ forecast_2 <- as.vector(fitted(model_2))
 forecast_3 <- as.vector(fitted(model_3))
 forecast_4 <- as.vector(fitted(model_4))
 
-# Find the minimum length among the forecast vectors
+# Find the minimum length
 min_length <- min(length(forecast_1), length(forecast_2), length(forecast_3), length(forecast_4))
 
-# Trim the forecast vectors to the minimum length
+# Trim the forecast vectors
 forecast_1 <- forecast_1[1:min_length]
 forecast_2 <- forecast_2[1:min_length]
 forecast_3 <- forecast_3[1:min_length]
 forecast_4 <- forecast_4[1:min_length]
 
-# Calculate equal weights
+# Calc Weights
 n_models <- 4
-weights <- rep(1/n_models, n_models)
-combined_forecast <- (forecast_1 + forecast_2 + forecast_3 + forecast_4) / n_models
+combined_forecast_ew <- (forecast_1 + forecast_2 + forecast_3 + forecast_4) / n_models
 
 fcst_ew <- numeric(27)
 ferror_ew <- numeric(27)
 loss_ew <- numeric(27)
 
-fcst_ew <- combined_forecast[(length(combined_forecast) - 26):length(combined_forecast)]
+fcst_ew <- combined_forecast_ew[(length(combined_forecast_ew) - 26):length(combined_forecast_ew)]
 
-for (i in 1:14) {
-  ferror_ew[i] <- train_log[length(train_log) - 14 + i] - fcast_ew[i]
+for (i in 1:27) {
+  ferror_ew[i] <- train_log[length(train_log) - 27 + i] - fcst_ew[i]
   loss_ew[i] <- ferror_ew[i]^2
 }
 
 MSE_ew <- mean(loss_ew)
 paste('MSE Combination Equal Weights Fcst:', MSE_ew)
 
-mpetest_ew <- lm(ferror_ew ~1)
+mpetest_ew <- lm(ferror_ew ~ 1)
 summary(mpetest_ew)
 IETest_ew <- lm(ferror_ew ~ fcst_ew)
 summary(IETest_ew) # Informal Efficiency Test Combination Equal Weights
@@ -400,19 +380,20 @@ forecast_2_iMSE <- as.vector(fitted(model_2))
 forecast_3_iMSE <- as.vector(fitted(model_3))
 forecast_4_iMSE <- as.vector(fitted(model_4))
 
-# Find the minimum length among the forecast vectors
+# Find the minimum length
 min_length <- min(length(forecast_1_iMSE), length(forecast_2_iMSE), length(forecast_3_iMSE), length(forecast_4_iMSE))
 
-# Trim the forecast vectors to the minimum length
-forecast_1_iMSE <- forecast_1[1:min_length]
-forecast_2_iMSE <- forecast_2[1:min_length]
-forecast_3_iMSE <- forecast_3[1:min_length]
-forecast_4_iMSE <- forecast_4[1:min_length]
+# Trim the forecast vectors
+forecast_1_iMSE <- forecast_1_iMSE[1:min_length]
+forecast_2_iMSE <- forecast_2_iMSE[1:min_length]
+forecast_3_iMSE <- forecast_3_iMSE[1:min_length]
+forecast_4_iMSE <- forecast_4_iMSE[1:min_length]
 
 # Calculate inverse of MSE
 mse_inverse <- 1 / c(MSE1, MSE2, MSE3, MSE4)
 mse_inverse <- na.omit(mse_inverse)
 
+# Set up forecast model
 fcst_iMSE <- numeric(27)
 ferror_iMSE <- numeric(27)
 loss_iMSE <- numeric(27)
@@ -421,12 +402,10 @@ combined_iMSE_forecast <- forecast_1_iMSE + forecast_2_iMSE + forecast_3_iMSE + 
 
 fcst_iMSE <- combined_iMSE_forecast[(length(combined_iMSE_forecast) - 26):length(combined_iMSE_forecast)]
 
-for (i in 1:14) {
-  ferror_iMSE[i] <- train_log[length(train_log) - 14 + i] - fcast_iMSE[i]
+for (i in 1:27) {
+  ferror_iMSE[i] <- train_log[length(train_log) - 27 + i] - fcst_iMSE[i]
   loss_iMSE[i] <- ferror_iMSE[i]^2
 }
-
-MSE_iMSE <- mean(loss_iMSE)
 
 MSE_iMSE <- mean(loss_iMSE)
 paste('MSE Combination Equal Weights Fcst:', MSE_iMSE)
@@ -443,14 +422,14 @@ forecast_2_ols <- as.vector(fitted(model_2))
 forecast_3_ols <- as.vector(fitted(model_3))
 forecast_4_ols <- as.vector(fitted(model_4))
 
-# Find the minimum length among the forecast vectors
+# Find the minimum length
 min_length <- min(length(forecast_1_ols), length(forecast_2_ols), length(forecast_3_ols), length(forecast_4_ols))
 
-# Trim the forecast vectors to the minimum length
-forecast_1_ols <- forecast_1[1:min_length]
-forecast_2_ols <- forecast_2[1:min_length]
-forecast_3_ols <- forecast_3[1:min_length]
-forecast_4_ols <- forecast_4[1:min_length]
+# Trim the forecast vectors
+forecast_1_ols <- forecast_1_ols[1:min_length]
+forecast_2_ols <- forecast_2_ols[1:min_length]
+forecast_3_ols <- forecast_3_ols[1:min_length]
+forecast_4_ols <- forecast_4_ols[1:min_length]
 
 # Calculate equal weights
 combined_ols_forecast <- (forecast_1_ols + forecast_2_ols + forecast_3_ols + forecast_4_ols)
@@ -461,15 +440,15 @@ loss_ols <- numeric(27)
 
 fcst_ols <- combined_ols_forecast[(length(combined_ols_forecast) - 26):length(combined_ols_forecast)]
 
-for (i in 1:14) {
-  ferror_ols[i] <- train_log[length(train_log) - 14 + i] - fcst_ols[i]
+for (i in 1:27) {
+  ferror_ols[i] <- train_log[length(train_log) - 27 + i] - fcst_ols[i]
   loss_ols[i] <- ferror_ols[i]^2
 }
 
 MSE_ols <- mean(loss_ols)
 paste('MSE Combination Ordinary Lease Squares (OLS) Fcst:', MSE_ols)
 
-mpetest_ols <- lm(ferror_ols ~1)
+mpetest_ols <- lm(ferror_ols ~ 1)
 summary(mpetest_ols)
 IETest_ols <- lm(ferror_ols ~ fcst_ols)
 summary(IETest_ols) # Informal Efficiency Test Combination Equal Weights
